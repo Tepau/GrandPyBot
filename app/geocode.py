7fac00e2832089562cc8c7e6985aca96f9b40533
Tepau <malaury.lemaitresalmon@gmail.com>
2019-09-25 17:12:56 +0200

    maj

--- app/geocode.py
+++ app/geocode.py
@@ -6,7 +6,7 @@ class GoogleMap:
      a place through the api "googlemap\""""
 
     def __init__(self):
-        self.gmaps = googlemaps.Client(key=[KEY])
+        self.gmaps = googlemaps.Client(key=['KEY'])
 
 
     def find_adress(self, search):
